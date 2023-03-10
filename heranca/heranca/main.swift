ee1a1367b951cff0454cbc94d4f4ba1cef259f3c
Vitor Ernane <vitorernane@hotmail.com>
2023-03-09 22:29:43 -0300

    Subi o exercicio de Heranca

--- heranca/heranca/main.swift
+++ heranca/heranca/main.swift
@@ -9,3 +9,56 @@ import Foundation
 
 print("Hello, World!")
 
+/* DESAFIO PARA PASSAR PARA CASA
+
+ • Criar uma classe pai na qual tenha 3 caracteristicas e 2 ações.
+ • Criar 2 classes filhas distintas, porem ambas vão herdar da classe pai(super). Cada classe filha terá 3 caracteristicas especificas.
+ • A classe pai terá construtor para setar seus valores.
+ • Uma das classes filhas não deve conter construtor.
+ • Uma das classes filhas deve conter contrutor indicando os valores de todas as suas propriedades. */
+
+class Human {
+    
+    var skin: String
+    var hair: String
+    var age: Double
+    
+    init(skin: String, hair: String, age: Double) {
+        self.skin = skin
+        self.hair = hair
+        self.age = age
+    }
+    
+    func walk () {
+        print("This human walking")
+    }
+    
+    func drink () {
+        print("this human drinking")
+    }
+    
+}
+
+class Adult: Human {
+    
+    var whiteHair: Bool = true
+    var beard: Bool = true
+    var accounts: Bool = true
+    
+}
+
+
+class Young: Human {
+
+    var piercing: Bool
+    var tatto: Bool
+    var sense: String
+
+    init(piercing: Bool, tatto: Bool, sense: String, skin: String, hair: String, age: Double) {
+        self.piercing = piercing
+        self.tatto = tatto
+        self.sense = sense
+        super.init(skin: skin, hair: hair, age: age)
+    }
+
+}
