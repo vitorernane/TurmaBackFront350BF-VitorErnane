19a2562da0b385be58ed044d712e919747699c77
Vitor Ernane <vitorernane@hotmail.com>
2023-03-14 20:25:51 -0300

    Adicionando conteudo sobre a aula de polimorfismo

--- polimorfismo/polimorfismo/main.swift
+++ polimorfismo/polimorfismo/main.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 
-//MARK: - Polimorfismo
+// MARK: - Polimorfismo
+
+class Centauro {
+    func totalDeDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.3
+    }
+}
+
+class Loja1: Centauro {
+    override func totalDeDesconto(valorTotal: Double) -> Double {
+        return valorTotal * 0.1
+    }
+}
+
+class Loja2: Centauro {
+
+}
+
+
+var lojaDoCaio: Loja1 = Loja1()
+print(lojaDoCaio.totalDeDesconto(valorTotal: 1000))
+
+
+var lojaDoBruno: Loja2 = Loja2()
+print(lojaDoBruno.totalDeDesconto(valorTotal: 1000))
