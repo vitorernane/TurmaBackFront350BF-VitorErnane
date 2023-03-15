f39476a31b24618bbcc3c1e99857354e93d4fb72
Vitor Ernane <vitorernane@hotmail.com>
2023-03-14 22:08:24 -0300

    adicionando conteúdo do curso

--- encapsulamento/encapsulamento/main.swift
+++ encapsulamento/encapsulamento/main.swift
@@ -8,3 +8,30 @@
 import Foundation
 
 // MARK: - Encapsulamento
+
+// Private -> privado, ou seja, que não tenha acesso externo
+// Public -> publico, ou seja, que tenha acesso externo
+
+class Carro {
+    
+    private var modelo: String
+    
+    init(modelo: String) {
+        self.modelo = modelo
+    }
+    
+    public func getModelo() -> String {
+        return self.modelo
+    }
+    
+    public func setModelo(modelo: String) {
+        self.modelo = modelo
+    }
+    
+}
+
+var meuCarro: Carro = Carro(modelo: "BMW 320i")
+
+print(meuCarro.getModelo())
+meuCarro.setModelo(modelo: "La Ferrari")
+print(meuCarro.getModelo())
