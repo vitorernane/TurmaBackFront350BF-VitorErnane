80287c32b6b62e46907d7397cdafe38828cee44e
Vitor Ernane <vitorernane@hotmail.com>
2023-03-16 22:28:14 -0300

    Foi adicionado o projeto na pasta

---
+++ Ligacoes/Ligacoes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ligacoes
+//
+//  Created by Vitor Ernane Guedes on 16/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
