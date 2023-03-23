d1546283ef181c61d12e05bac01a6383dd9f645a
Vitor Ernane <vitorernane@hotmail.com>
2023-03-23 10:16:55 -0300

    Nesse commit, foi inserido o projeto dentro da pasta. Fiz o upload dos assets,  configurei todas as constraints de ImageView, textField, UIbutton e label, após isso fiz as ligações IBOutlets dos textFields e a ligação do IBAction do button cadastrar depois foram criadas as lógicas de acordo com o que o professor pediu no desafio

---
+++ desafioTextFieldDelegate2/desafioTextFieldDelegate2/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  desafioTextFieldDelegate2
+//
+//  Created by Vitor Ernane Guedes on 23/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var adressTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var passwordAgainTextField: UITextField!
+    
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.delegate = self
+        adressTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordAgainTextField.delegate = self
+        
+        nameTextField.placeholder = "Nome"
+        let placeHolderNameTextField = NSAttributedString (string: "Nome", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        nameTextField.attributedPlaceholder = placeHolderNameTextField
+        
+        //configuração das cores do Placeholder
+        
+        adressTextField.placeholder = "Endereço"
+        let placeHolderAdressTextField = NSAttributedString (string: "Endereço", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        adressTextField.attributedPlaceholder = placeHolderAdressTextField
+        
+        
+        passwordTextField.placeholder = "Senha"
+        let placeHolderPasswordTextField = NSAttributedString (string: "Nome", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        passwordTextField.attributedPlaceholder = placeHolderPasswordTextField
+        
+        passwordAgainTextField.placeholder = "Digite a senha novamente"
+        let placeHolderPasswordAgainTextField = NSAttributedString (string: "Nome", attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        passwordAgainTextField.attributedPlaceholder = placeHolderPasswordAgainTextField
+        
+        registerButton.layer.backgroundColor = UIColor(red: 0.906, green: 0, blue: 0.494, alpha: 1).cgColor
+        registerButton.layer.cornerRadius = 10
+        
+        
+    }
+
+    @IBAction func registerActionButton(_ sender: Any) {
+        
+        print("Cadastro realizado com sucesso!")
+        
+    }
+    
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        print("textFieldDidBeginEditing")
+        
+        textField.layer.borderColor = UIColor.systemCyan.cgColor
+        textField.layer.borderWidth = 5
+        textField.layer.cornerRadius = 5
+        
+    }
+    
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        textField.layer.borderWidth = 0
+        
+        // lógica para inserir a borda colorida, indicando se há ou não dados dentro do textfield.
+        
+       if textField.hasText == true {
+            textField.layer.borderWidth = 3
+            textField.layer.borderColor = UIColor.white.cgColor
+       } else {
+           textField.layer.borderColor = UIColor.red.cgColor
+           textField.layer.borderWidth = 3
+       }
+        
+        //lógica para validar se o texto digitado no campo senha é igual ao campo digite sua senha novamente, caso for igual, o botão é habilitado.
+        
+        if passwordTextField.text == passwordAgainTextField.text {
+            registerButton.isEnabled = true
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        print("textFieldShouldReturn")
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
+}
