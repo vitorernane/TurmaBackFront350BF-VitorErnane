257183b6b17b705a0063336ae0350e2c44b2be91
Vitor Ernane <vitorernane@hotmail.com>
2023-04-09 16:16:07 -0300

    Foi adicionado o arquivo, criado outra viewController e adicionado o ciclo de vida

---
+++ lifeCycle/lifeCycle/tela02ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  tela02ViewController.swift
+//  lifeCycle
+//
+//  Created by Vitor Ernane Guedes on 09/04/23.
+//
+
+import UIKit
+
+class tela02ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print(#function)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print(#function)
+    }
+    
+
+}
