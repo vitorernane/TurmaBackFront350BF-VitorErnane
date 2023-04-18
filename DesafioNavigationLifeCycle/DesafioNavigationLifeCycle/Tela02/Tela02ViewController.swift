b33ae01631e1e6ca32aeb6e4835695d82d6f007d
Vitor Ernane <vitorernane@hotmail.com>
2023-04-18 20:54:48 -0300

    foram configuradas a viewcontroller e feito a navegacao com transiçao de informacoes de uma tela para outra

--- DesafioNavigationLifeCycle/DesafioNavigationLifeCycle/Tela02/Tela02ViewController.swift
+++ DesafioNavigationLifeCycle/DesafioNavigationLifeCycle/Tela02/Tela02ViewController.swift
@@ -8,22 +8,32 @@
 import UIKit
 
 class Tela02ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    let name: String
+    
+    init?(coder: NSCoder, name:String){
+        self.name = name
+        super .init(coder: coder)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = name
+    }
+    
+    @IBAction func tappedBackButton(_ sender: UIButton) {
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
 
 }
--- DesafioNavigationLifeCycle/DesafioNavigationLifeCycle/ViewController.swift
+++ DesafioNavigationLifeCycle/DesafioNavigationLifeCycle/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  
     }
 
-
+    @IBAction func tappedShowTela02Button(_ sender: UIButton) {
+        
+        let vc = UIStoryboard(name: "Tela02ViewController", bundle: nil).instantiateViewController(identifier: "Tela02ViewController", creator:{ coder -> Tela02ViewController? in
+            return Tela02ViewController(coder: coder, name: self.nameTextField.text ?? "")
+        })
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
 }
 
